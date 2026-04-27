912b94c0cd20c09f4295508c5c9ac902659295c5
Sandesh Lande <150541370+sandeshlande2004@users.noreply.github.com>
2026-04-27 11:09:01 +0530

    Time: 913 ms (35.19%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select Employee.name, Bonus.bonus
+from Employee left join Bonus
+on Employee.empId = Bonus.empId
+where bonus<1000 or
+      bonus is null;
